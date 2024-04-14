e25882149ba4002b8b891aef2012ae8dfc0b6566
1shakti <shakti26.sj@gmail.com>
2024-04-14 19:10:28 +0530

    rmv ball canvas kept image only, added computer and star canvas

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -5,10 +5,10 @@ import { technologies } from '../constants';
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10 ' >
-      {technologies.map((technology,index) => (
-        <div className=' w-28 h-28 ' key={index} >
-          <BallCanvas  icon={technology.icon} />
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology, index) => (
+        <div className='w-20 h-20 m-4' key={index}>
+          <BallCanvas icon={technology.icon} />
         </div>
       ))}
     </div>
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,31 +1,18 @@
 import React, { Suspense, useEffect, useRef, useState } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
 import CanvasLoader from "../Loader";
 
 const Ball = ({ imgUrl }) => {
-  const [decal] = useTexture([imgUrl]);
-
   return (
-    <Float speed={3} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.25]} />
-      <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial
-          color="#FFF8eb"
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
+    <div className="rounded-full flex justify-center items-center border-gray-300 relative w-20 h-20 bg-gradient-to-br from-blue-200 to-purple-200 shadow-lg m-4">
+      <div>
+        <img
+          src={imgUrl}
+          alt={`Icon ${imgUrl}`}
+          className="icon w-12 h-12 object-contain"
+          loading='lazy'
         />
-        <Decal
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          map={decal}
-          flatShading
-        />
-      </mesh>
-    </Float>
+      </div>
+    </div>
   );
 };
 
@@ -54,13 +41,9 @@ const BallCanvas = ({ icon }) => {
   return (
     <div ref={ballRef}>
       {isIntersecting && (
-        <Canvas gl={{ preserveDrawingBuffer: true }}>
           <Suspense fallback={<CanvasLoader />}>
-            <OrbitControls enableZoom={false} />
             <Ball imgUrl={icon} />
           </Suspense>
-          <Preload all />
-        </Canvas>
       )}
     </div>
   );
